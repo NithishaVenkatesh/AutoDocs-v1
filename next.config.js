8de42882fe7b12e5ef9bdc5cfe70b62323db7c4c
NithishaVenkatesh <nithishaleni1806@gmail.com>
2025-10-15 10:38:30 +0530

    Fix build: disable Turbopack and switch to npm

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: false, // disables Turbopack; use Webpack instead
+  },
 };
 
 module.exports = nextConfig;
